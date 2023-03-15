dd76b9cb07eea2178371b33a721889e90eca09ce
unknown <keibin.wru@gmail.com>
2023-03-15 16:31:01 +0700

    2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2d = new Point2D();
+        System.out.println("mảng 2d: "+ point2d);
+
+        Point3D point3D = new Point3D();
+        System.out.println("mảng 3d: "+ point3D);
+    }
+}
---
+++ src/Point3D.java
@@ -0,0 +1,37 @@
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D() {
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public float[] getXYZ() {
+        return new float[]{super.getX(), super.getY(), z};
+    }
+
+    public void setZ(float x, float y, float z) {
+        this.z = z;
+        this.y = y;
+        this.x = z;
+    }
+
+    @Override
+    public String toString() {
+        return "Point3D{" +
+                "z=" + z +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
